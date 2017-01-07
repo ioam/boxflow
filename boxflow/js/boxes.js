a7b8aa8424260f7e0189fc90f6be6695330fa7d6
jlstevens <jlstevens@ed.ac.uk>
2017-01-07 22:55:18 +0000

    Removed incorrect increment to header_height in LabelledBox

--- boxflow/js/boxes.js
+++ boxflow/js/boxes.js
@@ -114,7 +114,6 @@ class LabelledBox extends BaseBox {
         }
         // Allocate space in the node neader
         node.title_height = title.top + (title.height * title.scaleY);
-        node.header_height += node.title_height;
 
         return title
     }
