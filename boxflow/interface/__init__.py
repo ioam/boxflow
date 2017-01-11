df2b1c0509f61dcf9cc62b9e83c6a9915d5a2b3e
jlstevens <jlstevens@ed.ac.uk>
2017-01-11 11:58:31 +0000

    Simplified conditional import handling

--- boxflow/interface/__init__.py
+++ boxflow/interface/__init__.py
@@ -1,10 +1,6 @@
 # Loads extension classes from supported libraries
 
 from __future__ import absolute_import
-
-def unsupported():
-    return
-
 from.interface import Interface
 
 from .param import load_param, param_display               # Param is a core dependency
@@ -15,19 +11,23 @@ try:
     import imagen
     from .imagen import load_imagen, imagen_display
 except:
-    load_imagen = unsupported
+    load_imagen = None
 
 
 try:
     import holoviews
     from .holoviews import load_holoviews
 except:
-    load_holoviews = unsupported
+    load_holoviews = None
 
 load_param()
 load_numbergen()
-load_imagen()
-load_holoviews()
+
+if load_imagen:
+    load_imagen()
+
+if load_holoviews:
+    load_holoviews()
 
 __all__ = ['Interface',
            'param_display',
