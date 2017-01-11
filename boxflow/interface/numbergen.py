2e09579c87876f44d169b66b36a931f28e0a8723
jlstevens <jlstevens@ed.ac.uk>
2017-01-11 11:30:18 +0000

    Added BinaryOperator to interface.numbergen

--- boxflow/interface/numbergen.py
+++ boxflow/interface/numbergen.py
@@ -65,6 +65,20 @@ class Subtract(param.Parameterized):
         return self.lhs - self.rhs
 
 
+class BinaryOperator(numbergen.NumberGenerator):
+
+    lhs = param.Number(default=0)
+
+    rhs = param.Number(default=1)
+
+    operator = param.ObjectSelector(objects=['add','sub','mul','mod','pow',
+                                             'div','truediv','floordiv'])
+
+    def __call__(self):
+        op = getattr(operator, self.operator)
+        return op(self.lhs() if callable(self.lhs) else self.lhs,
+                  self.rhs() if callable(self.rhs) else self.rhs)
+
 
 def numbergen_nodes():
     return {'LabelledNode':[Percentage, Magnitude, Multiply, Divide, Add, Subtract]}
